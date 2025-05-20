7f3ad326878e382a7a3f532b2d7fd5061c89ffae
soolyshine <anna.krasnobaieva@infiz.khpi.edu.ua>
2025-05-20 18:55:36 +0300

    Add files via upload

--- src/components/A.jsx
+++ src/components/A.jsx
@@ -1,1 +1,18 @@
+import React from "react";
+import { increment, decrement, incrementByAmount } from "../counterSlice";
+import { useDispatch } from "react-redux";
 
+function A() {
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <h1>Comp A: controls</h1>
+            <button onClick={() => dispatch(increment())}>+1</button>
+            <button onClick={() => dispatch(decrement())}>-1</button>
+            <button onClick={() => dispatch(incrementByAmount(5))}>+5</button>
+        </div>
+    );
+}
+
+export default A;
